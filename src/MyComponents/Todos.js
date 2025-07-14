239d1b12f906e799aec0dc4f9bcc9b70d8d7f6ee
Sheetal Sharma <sheetal11sharma86@gmail.com>
2025-07-14 11:44:47 -0700

    initial commit

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import {Todoitem} from "./Todoitem";
+
+import React from 'react';
+import { Todoitem } from "./Todoitem";
 
 export const Todos = (props) => {
-  const myStyle = {
-    minHeight: "70vh",
-    margin: "40px auto"
-  };
   return (
-    
-    <div className="container"> 
-    <h3 className="my-3">Todos List</h3>
-    {props.todos.length===0? "no todos to display":
-    props.todos.map((todo)=>{
-    return (
-    <> 
-    < Todoitem todo ={todo} key={todo.sno} onDelete={props.onDelete}/><hr/>
-    </>
-    )
-    })
-    }
+    <div className="container">
+      <h3 className="my-3">Todos List</h3>
+      {props.todos.length === 0 ? (
+        <p>No todos to display</p>
+      ) : (
+        props.todos.map((todo) => (
+          <div key={todo.sno}>
+            <Todoitem todo={todo} onDelete={props.onDelete} />
+            <hr />
+          </div>
+        ))
+      )}
     </div>
-  )
-  
-}
+  );
+};
