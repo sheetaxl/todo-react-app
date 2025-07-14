61fd2809f15bcd2951a334045c3e234f2a7652ed
Sheetal Sharma <sheetal11sharma86@gmail.com>
2025-07-14 11:25:03 -0700

    Fix ESLint warnings and href issues

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -10,7 +10,7 @@ export const Todos = (props) => {
     
     <div className="container"> 
     <h3 className="my-3">Todos List</h3>
-    {props.todos.length==0? "no todos to display":
+    {props.todos.length===0? "no todos to display":
     props.todos.map((todo)=>{
     return (
     <> 
